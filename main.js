9f2e11bfcd93d3608c7a5c4e37c6f7fbbda99e02
root <psvoltaire96@gmail.com>
2019-12-01 12:38:33 -0500

    fixed timer

--- main.js
+++ main.js
@@ -2,25 +2,46 @@ const Timer = require('tiny-timer')
 var Enum = require('enum');
 const timer = new Timer()
 
-var States = new Enum({'AWAKE':0,'REM':1, 'N1':2, 'N2':3,'N3':4,'N4':5});
+var States = new Enum({
+    'AWAKE': 0,
+    'REM': 1,
+    'N1': 2,
+    'N2': 3,
+    'N3': 4,
+    'N4': 5
+});
 
-const ms=1000/5
-const Hour=60/60
-const minute=Hour/60;
-let currentState=States.get(0).key
+const Hour = 60 / 10
+const minute = Hour / 60;
+const seconds = minute / 60;
+const ms = seconds/2
+const dataTransferRate=500*ms
+
+let hoursElapsed = 0
+
+let currentState = States.get(0).key
 
 
-timer.on('tick', (ms) => {
-    let nextI=States.get(currentState).value+1
-    console.log(nextI)
+setInterval(() => {
+    hoursElapsed = +1
+}, Hour * 1000)
 
-    if(nextI>States.size){
-nextI=0
+
+timer.on('tick', (dataTransferRate) => {
+    let nextI = States.get(currentState).value + 1
+    if (nextI > States.size) {
+        nextI = 0
     }
 
-    currentState=States.get(nextI).key
-currentDataPoint={"State":currentState,"date":new Date().toUTCString}
-    console.log(currentDataPoint, ms)})
+    currentState = States.get(nextI).key
+    currentDate = new Date();
+
+    currentDate.setHours(currentDate.getHours() + hoursElapsed);
+    currentDataPoint = {
+        "State": currentState,
+        "date": currentDate.toUTCString()
+    }
+    console.log(currentDataPoint, ms)
+})
 timer.on('done', () => console.log('done!'))
-timer.start(15000)
-console.log("d")
+timer.start((60*60*60)*1000)
