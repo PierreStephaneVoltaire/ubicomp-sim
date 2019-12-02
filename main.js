a25e6442020af92c7cf2d3025e7d0796898bd6a3
root <psvoltaire96@gmail.com>
2019-12-02 00:12:05 -0500

    done with sleep timer

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 const Timer = require('tiny-timer')
 var Enum = require('enum');
+const fastify = require('fastify')({
+    logger: true
+})
 const timer = new Timer()
-
-var States = new Enum({
+const MQTT = require("async-mqtt");
+const States = new Enum({
     'AWAKE': 0,
     'REM': 1,
     'N1': 2,
@@ -11,37 +14,75 @@ var States = new Enum({
     'N4': 5
 });
 
-const Hour = 60 / 10
-const minute = Hour / 60;
-const seconds = minute / 60;
-const ms = seconds/2
-const dataTransferRate=500*ms
+run()
 
-let hoursElapsed = 0
+async function run() {
+    const client = await MQTT.connectAsync("tcp://192.168.2.60:1883")
+    //a minute is an hour here
+    const seconds = 100;
+    const ms = seconds / 2
+    const dataTransferRate = 500 * ms
 
-let currentState = States.get(0).key
+    let hoursElapsed = 0
+    let minutesElapsed = 0
+    let secondsElapsed = 0
+    let currentState = States.get(0).key
 
 
-setInterval(() => {
-    hoursElapsed = +1
-}, Hour * 1000)
+    setInterval(() => {
+        if (secondsElapsed < 60) {
+            secondsElapsed += 1
+        } else if (minutesElapsed < 60) {
+            secondsElapsed = 0
+            minutesElapsed += 1
+        } else {
+            secondsElapsed = 0
+            minutesElapsed = 0
+            hoursElapsed += 1
+        }
+    }, seconds)
 
+    timer.on('tick', async (dataTransferRate) => {
+        let nextI = States.get(currentState).value + 1
+        if (nextI > States.size) {
+            nextI = 1
+        }
 
-timer.on('tick', (dataTransferRate) => {
-    let nextI = States.get(currentState).value + 1
-    if (nextI > States.size) {
-        nextI = 0
-    }
+        currentState = States.get(nextI).key
+        currentDate = new Date();
 
-    currentState = States.get(nextI).key
-    currentDate = new Date();
+        currentDate.setHours(currentDate.getHours() + hoursElapsed, currentDate.getMinutes() + minutesElapsed, currentDate.getSeconds() + secondsElapsed);
+        currentDataPoint = {
+            "State": currentState,
+            "date": currentDate.toUTCString()
+        }
+        try {
+            await client.publish("test_topic", JSON.stringify(currentDataPoint));
+            // This line doesn't run until the server responds to the publish
 
-    currentDate.setHours(currentDate.getHours() + hoursElapsed);
-    currentDataPoint = {
-        "State": currentState,
-        "date": currentDate.toUTCString()
-    }
-    console.log(currentDataPoint, ms)
-})
-timer.on('done', () => console.log('done!'))
-timer.start((60*60*60)*1000)
+        } catch (e) {
+            // Do something about it!
+            console.log(e.stack);
+            process.exit();
+        }
+    })
+    timer.on('done', async () => {
+        let currentDate = new Date();
+
+        currentDate.setHours(currentDate.getHours() + hoursElapsed, currentDate.getMinutes() + minutesElapsed, currentDate.getSeconds() + secondsElapsed);
+        let currentDataPoint = {
+            "State": States.get(0).key,
+            "date": currentDate.toUTCString()
+        }
+        try {
+            await client.publish("test_topic", JSON.stringify(currentDataPoint));
+            // This line doesn't run until the server responds to the publish
+
+        } catch (e) {
+            // Do something about it!
+            console.log(e.stack);
+            process.exit();
+        }
+    })
+    timer.start(seconds*60*60*8)
+}
